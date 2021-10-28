8dcaf157ec83b0929e9dcfda4fe70cb66e7b31b3
Luffy-dalock <jongho@2ndsyndrome.com>
2021-10-28 13:41:44 +0900

    setting prettier

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  env: {
+    node: true,
+    browser: true,
+    es2021: true,
+  },
+  extends: ['eslint:recommended', 'prettier'],
+
+  plugins: ['import', 'prettier'],
+  rules: {},
+};
